923b96267bb9fc096e2a0e6e7515f149167f7875
BakiDag <73464615+BakiDag@users.noreply.github.com>
2022-09-25 14:54:47 +0200

    Add project files.

---
+++ ScreenOnOff/ScreenOnOff/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace ScreenOnOff
+{
+    public partial class MainPage : ContentPage
+    {       
+        public MainPage()
+        {
+            InitializeComponent();
+            DeviceDisplay.KeepScreenOn = true;
+        }
+
+        private async Task OnClickedAsync(object sender, EventArgs e)
+        {
+            await DisplayAlert("Test Titel", "Testmessage here", "ok");
+        }
+        private async void OnButtonClicked(object sender, EventArgs args)
+        {   
+            DeviceDisplay.KeepScreenOn = true;
+            //DeviceDisplay.KeepScreenOn = false;
+            var x = DeviceDisplay.KeepScreenOn;
+            await DisplayAlert("Test Titel", x.ToString(), "ok");             
+        }
+    }
+}
